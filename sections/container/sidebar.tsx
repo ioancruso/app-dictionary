5d8c6b8eb0b8e6ba6c5fb9978e395f74d037001e
Crușoveanu Ioan <ionut.cruso@gmail.com>
2023-11-11 13:18:29 +0200

    - minor modification

--- sections/container/sidebar.tsx
+++ sections/container/sidebar.tsx
@@ -5,12 +5,12 @@ import {Button} from "@/components/button/button";
 function Sidebar() {
     return (
         <>
-            <div className={styles.sidebar}>
+            <aside className={styles.sidebar}>
                 <div className={styles.addExpression}>
                     <h2>Poți contribui și tu la acest dicționar</h2>
                     <Button text="Adaugă o expresie" href="/adauga" />
                 </div>
-            </div>
+            </aside>
         </>
     );
 }
